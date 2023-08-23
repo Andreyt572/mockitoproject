15608334fc5358530ca073d58f810eacbe81558f
Andrey Tolokonnikov <andrey572@mail.ru>
2023-08-23 19:43:45 +0300

    try2

--- src/test/java/ru/netology/mockitoproject/MovieManagerTest.java
+++ src/test/java/ru/netology/mockitoproject/MovieManagerTest.java
@@ -34,4 +34,19 @@ public class MovieManagerTest {
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void test4() {
+        MovieManager manager = new MovieManager();
+        manager.add("Film1");
+        manager.add("Film2");
+        manager.add("Film3");
+        manager.add("Film4");
+        manager.add("Film5");
+        manager.add("Film6");
+        manager.add("Film7");
+        String[] expected ={ "Film1", "Film2", "Film3", "Film4", "Film5", "Film6", "Film7"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
